50383a891f7a0fdc6444a1c20b98a9a5981a50ac
Vatt Sopheak <vattcarter@gmail.com>
2024-02-16 13:04:12 +0700

    verifyUserSchema

--- src/modules/users/users.schemas.ts
+++ src/modules/users/users.schemas.ts
@@ -1,43 +1,44 @@
-import z from "zod";
+import zod, { object, string } from "zod";
 
-export const loginWithEmailAndPassword = z.object({
-  body: z.object({
-    email: z
-      .string({
-        required_error: "Email is required",
-      })
-      .email("Invalid email or password"),
-    password: z
-      .string({
-        required_error: "Password is required",
-      })
-      .min(6, "Invalid email or password"),
+export const loginWithEmailAndPassword = object({
+  body: object({
+    email: string({
+      required_error: "Email is required",
+    }).email("Invalid email or password"),
+    password: string({
+      required_error: "Password is required",
+    }).min(6, "Invalid email or password"),
   }),
 });
 
-export const createUserSchema = z.object({
-  body: z.object({
-    firstName: z.string({
+export const createUserSchema = object({
+  body: object({
+    firstName: string({
       required_error: "First name is required",
     }),
-    lastName: z.string({
+    lastName: string({
       required_error: "Last name is required",
     }),
-    email: z
-      .string({
-        required_error: "Email is required",
-      })
-      .email("Not a valid email"),
-    password: z
-      .string({
-        required_error: "Password is required",
-      })
-      .min(6, "Password is too short - at least 6 characters"),
+    email: string({
+      required_error: "Email is required",
+    }).email("Not a valid email"),
+    password: string({
+      required_error: "Password is required",
+    }).min(6, "Password is too short - at least 6 characters"),
   }),
 });
 
-export type LoginWithEmailAndPasswordInput = z.infer<
+export const verifyUserSchema = object({
+  params: object({
+    id: string(),
+    verificationCode: string(),
+  }),
+});
+
+export type LoginWithEmailAndPasswordInput = zod.infer<
   typeof loginWithEmailAndPassword
 >["body"];
 
-export type CreateUserInput = z.infer<typeof createUserSchema>["body"];
+export type CreateUserInput = zod.infer<typeof createUserSchema>["body"];
+
+export type VerifyUserInput = zod.infer<typeof verifyUserSchema>["params"];
