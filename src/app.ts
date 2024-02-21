167a8259fb68cdaadfaaa4cac52a068849db3a25
Vatt Sopheak <vattcarter@gmail.com>
2024-02-21 17:04:19 +0700

    Catch all routes error

--- src/app.ts
+++ src/app.ts
@@ -25,15 +25,8 @@ const main = async () => {
 
   app.use(userRoute);
 
-  app.all("*", (req, res) => {
-    res.status(404);
-    if (req.accepts("html")) {
-      res.sendFile(path.join(__dirname, "views", "404.html"));
-    } else if (req.accepts("json")) {
-      res.json({ error: "404 Not Found" });
-    } else {
-      res.type("txt").send("404 Not Found");
-    }
+  app.all("*", (_, res) => {
+    return res.status(404).json({ error: "404: Route Not Found" });
   });
 
   app.use(errorHandler);
